ea35c9bd6d5e982f77d86241552d57fb9bd32a6a
Joachim Fasting <joachim.fasting@gmail.com>
2009-08-21 14:18:04 +0200

    Rewrite

--- Main.hs
+++ Main.hs
@@ -1,29 +1,47 @@
-import Data.List
-import System.FilePath
-import Data.ByteString.Char8 (ByteString)
-import qualified Data.ByteString.Char8 as B
-import System.Posix
+{-# LANGUAGE NoMonomorphismRestriction #-}
 
-import Network.MPD
+-- |
+-- Module      : Main
+-- Copyright   : (c) Joachim Fasting 2009
+-- License     : GPL-2 (see COPYING)
+--
+-- Maintainer  : joachim.fasting@gmail.com
+-- Stability   : unstable
+-- Portability : not portable
+--
+-- A FUSE filesystem for the Music Player Daemon (MPD).
+-- See README for more information.
+
+module Main (main) where
+
+import Data.List (isPrefixOf)
+import Control.Monad (join, liftM)
+import qualified Data.ByteString.Char8 as B
+import Data.ByteString.Char8 (ByteString)
+import System.FilePath ((</>), takeBaseName, takeDirectory, splitDirectories)
 import System.Fuse
+import System.Posix hiding (createDirectory, rename)
+import Network.MPD hiding (rename)
+import qualified Network.MPD as M
+import Prelude hiding (readFile, writeFile)
 
 main :: IO ()
-main = fuseMain mpdFSOps defaultExceptionHandler
+main = fuseMain operations defaultExceptionHandler
 
 --
--- File system operations.
+-- FUSE operations.
 --
 
-mpdFSOps :: FuseOperations fh
-mpdFSOps = defaultFuseOps
-    { fuseGetFileStat = mpdGetFileStat
-    , fuseRead = mpdRead
-    , fuseWrite = mpdWrite
-    , fuseOpen = mpdOpen
-    , fuseReadDirectory = mpdReadDirectory
-    , fuseOpenDirectory = mpdOpenDirectory
-    , fuseCreateDirectory = mpdCreateDirectory
-    , fuseRename = mpdRename
+operations :: FuseOperations fh
+operations = defaultFuseOps
+    { fuseGetFileStat = stat
+    , fuseOpen = openFile
+    , fuseRead = readFile
+    , fuseWrite = writeFile
+    , fuseReadDirectory = readDir
+    , fuseOpenDirectory = openDirectory
+    , fuseCreateDirectory = createDirectory
+    , fuseRename = rename
     -- Dummies to make FUSE happy.
     , fuseSetFileSize = (\_ _ -> return eOK)
     , fuseSetFileTimes = (\_ _ _ -> return eOK)
@@ -31,172 +49,176 @@ mpdFSOps = defaultFuseOps
     , fuseSetOwnerAndGroup = (\_ _ _ -> return eOK)
     }
 
--- Determines what is returned by the readdir(3) call.
---
--- To add a new MPDFS directory make sure it has an entry here, in
--- 'mpdOpenDirectory' and in 'mpdGetFileStat'.
-mpdReadDirectory :: FilePath -> IO (Either Errno [(FilePath, FileStat)])
-mpdReadDirectory "/" = return $ Right
-    [(".", mpdDirectory)
-    ,("..", mpdDirectory)
-    ,("Outputs", mpdDirectory)
-    ,("Stats", mpdDirectory)]
-mpdReadDirectory "/Outputs" = do
-    r <- withMPD outputs
-    case r of
-        Right os -> return . Right $
-            [(".", mpdDirectory)
-            ,("..", mpdDirectory)]
-            ++ map (\x -> (outputFileName x, mpdWritableFile)) os
-        Left e   -> do
-               print e -- debug message, visible when mounted with '-d'
-               return $ Left eNOENT
-mpdReadDirectory "/Stats" = do
-    r <- withMPD ping
-    case r of
-        Right _ -> return . Right $
-           [(".", mpdDirectory)
-           ,("..", mpdDirectory)
-           ,("artists", mpdReadOnlyFile)
-           ,("albums", mpdReadOnlyFile)
-           ,("songs", mpdReadOnlyFile)
-           ,("uptime", mpdReadOnlyFile)
-           ,("playtime", mpdReadOnlyFile)
-           ,("db_playtime", mpdReadOnlyFile)
-           ,("db_update", mpdReadOnlyFile)]
-        Left e -> do
-               print e
-               return $ Left eNOENT
-mpdReadDirectory _ = return $ Left eNOENT
+openDirectory :: FilePath -> IO Errno
+openDirectory p = do
+    putStrLn $ "OPEN DIRECTORY: " ++ p
+    st <- stat p
+    case st of
+        Right st' -> case statEntryType st' of
+                         Directory -> return eOK
+                         _         -> return eNOTDIR
+        _         -> return eNOENT
 
--- Determines what happens when someone requests to open a directory
--- in the file system.
-mpdOpenDirectory :: FilePath -> IO Errno
-mpdOpenDirectory p
-    | p `elem` ["/", "/Outputs", "/Stats"] = return eOK
-mpdOpenDirectory _ = return eNOENT
+createDirectory :: FilePath -> FileMode -> IO Errno
+createDirectory p _ = do
+    putStrLn $ "CREATE DIRECTORY: " ++ p
+    return eOK
 
-mpdCreateDirectory :: FilePath -> FileMode -> IO Errno
-mpdCreateDirectory _ _ = return eOK
+rename :: FilePath -> FilePath -> IO Errno
+rename p newName = do
+    putStrLn $ "RENAME DIRECTORY: " ++ p ++ " " ++ newName
+    return eOK
 
-mpdRename :: FilePath -> FilePath -> IO Errno
-mpdRename _ _ = return eOK
+-- Implements the readdir(3) call.
+readDir :: FilePath -> IO (Either Errno [(FilePath, FileStat)])
+readDir p = do
+    putStrLn $ "READ DIRECTORY: " ++ p
+    Right `liftM` getDirectoryContents p
 
--- Determines what is returned by the open(3) call.
---
--- To add a new file to MPDFS make sure it has an entry here, in 'mpdRead',
---'mpdWrite', 'mpdOpen', and 'mpdGetFileStat'.
-mpdRead :: FilePath -> fh -> ByteCount -> FileOffset -> IO (Either Errno ByteString)
-mpdRead p _ _ _
-    | "/Outputs" `isPrefixOf` p = do
-    r <- withMPD outputs
-    case r of
-        Right os -> do
-            let [d] = filter ((==) (takeOutputID p) . dOutputID) os
-            print d
-            return . Right $ B.pack ((if dOutputEnabled d then "1" else "0") ++ "\n")
-        Left e -> do
-            print e
-            return $ Left eNOENT
-    | "/Stats" `isPrefixOf` p = do
-    let fileName = takeBaseName p
-        field    = case fileName of
-                       "artists"     -> stsArtists
-                       "albums"      -> stsAlbums
-                       "songs"       -> stsSongs
-                       "uptime"      -> stsUptime
-                       "playtime"    -> stsPlaytime
-                       "db_playtime" -> stsDbPlaytime
-                       "db_update"   -> stsDbUpdate
-    r <- withMPD stats
-    case r of
-        Right sts -> return . Right . B.pack $ show (field sts) ++ "\n"
-        Left e -> do
-               print e
-               return $ Left eNOENT
+-- Implements the open(3) call.
+openFile :: FilePath -> OpenMode -> OpenFileFlags -> IO (Either Errno fh)
+openFile p _ _ = do
+    putStrLn $ "OPEN FILE: " ++ p
+    st <- stat p
+    case st of
+        Right st' -> case statEntryType st' of
+                         RegularFile -> return $ Right undefined
+                         _           -> return $ Left eNOENT
+        _         -> return $ Left eNOENT
 
-mpdRead _ _ _ _ = return $ Left eNOENT
+-- Implements the read(3) call.
+-- XXX: needs cleanup
+readFile :: FilePath -> fh -> ByteCount -> FileOffset
+         ->IO (Either Errno ByteString)
+readFile p _ _ _ = do
+    putStrLn $ "READ FILE " ++ p
+    r <- case splitDirectories ("/" </> p) of
+             ("/Outputs":_:[]) -> readDeviceFile p
+             ("/Stats":_:[])   -> readStatsFile p
+             _                     -> fail "No such file"
+    return $ mpdToErrno r
+
+readDeviceFile p = withMPD $ do
+   xs <- outputs
+   case filter ((==) (takeDeviceID p) . dOutputID) xs of
+       [d] -> return . B.pack $
+              if dOutputEnabled d then "1" else "0" ++ "\n"
+
+readStatsFile p = withMPD $ do
+    case lookup (takeBaseName p) selectors of
+        Just f  -> (flip B.snoc '\n' . packInt . f) `liftM` stats
+        Nothing -> fail "No such file"
+    where
+        selectors = [("artists", stsArtists)
+                    ,("albums", stsAlbums)
+                    ,("songs", stsSongs)
+                    ,("uptime", stsUptime)
+                    ,("db_playtime", stsDbPlaytime)
+                    ,("db_update", stsDbUpdate)]
 
 -- Implements the pwrite(2) call.
-mpdWrite :: FilePath -> fh -> ByteString -> FileOffset -> IO (Either Errno ByteCount)
-mpdWrite p _ s _
-    | "/Outputs" `isPrefixOf` p = do
-    let newState = case B.readInt s of Just (0, _) -> disableOutput
-                                       _           -> enableOutput
-    print s
-    print p
-    r <- withMPD $ newState (takeOutputID p)
-    case r of
-        Right _ -> return $ Right (fromIntegral $ B.length s)
-        Left e  -> do
-            print e
-            return $ Left eNOENT
+-- XXX: needs cleanup
+writeFile :: FilePath -> fh -> ByteString -> FileOffset -> IO (Either Errno ByteCount)
+writeFile p _ s _ = do
+    putStrLn $ "WRITE FILE" ++ p
 
-mpdWrite _ _ _ _ = return $ Left eNOENT
+    r <- case splitDirectories ("/" </> p) of
+             ("/Outputs":_:[]) -> writeDeviceFile p s
+             _                     -> fail "No such file"
 
-mpdOpen :: FilePath -> OpenMode -> OpenFileFlags -> IO (Either Errno fh)
-mpdOpen p _ _
-    | "/Outputs" `isPrefixOf` p = return $ Right undefined
-    | "/Stats" `isPrefixOf` p   = return $ Right undefined
-mpdOpen _ _ _               = return $ Left eNOENT
+    return $ either (const $ Left eNOENT)
+                    (const $ Right . fromIntegral $ B.length s)
+                    r
+
+writeDeviceFile p s = withMPD $ do
+    let setState = case B.readInt s of Just (0, _) -> disableOutput
+                                       _ -> enableOutput
+    setState (takeDeviceID p)
 
 -- Implements the stat(3) call.
---
--- Remember that read(3) will not read more than 'statFileSize'.
-mpdGetFileStat :: FilePath -> IO (Either Errno FileStat)
-mpdGetFileStat "/" = do
-    ctx <- getFuseContext
-    return . Right $ mpdDirectory
-        { statFileOwner = fuseCtxUserID ctx
-        , statFileGroup = fuseCtxGroupID ctx
-        }
-mpdGetFileStat "/Outputs" = do
-    ctx <- getFuseContext
-    return . Right $ mpdDirectory
-        { statFileOwner = fuseCtxUserID ctx
-        , statFileGroup = fuseCtxGroupID ctx
-        }
-mpdGetFileStat "/Stats" = do
-    ctx <- getFuseContext
-    return . Right $ mpdDirectory
-        { statFileOwner = fuseCtxUserID ctx
-        , statFileGroup = fuseCtxGroupID ctx
-        }
-mpdGetFileStat p
-    | "/Outputs" `isPrefixOf` p = do
-    ctx <- getFuseContext
-    return . Right $ mpdWritableFile
-        { statFileOwner = fuseCtxUserID ctx
-        , statFileGroup = fuseCtxGroupID ctx
-        , statFileSize  = 4096
-        }
-    | "/Stats" `isPrefixOf` p = do
-    ctx <- getFuseContext
-    return . Right $ mpdWritableFile
-        { statFileOwner = fuseCtxUserID ctx
-        , statFileGroup = fuseCtxGroupID ctx
-        -- XXX: we should use the actual size of the contents here
-        , statFileSize  = 4096
-        }
-mpdGetFileStat _ = return $ Left eNOENT
+stat :: FilePath -> IO (Either Errno FileStat)
+stat "/" = return $ Right directory
+stat p = do
+    putStrLn $ "STAT DIRECTORY: " ++ p
+    cs <- getDirectoryContents (takeDirectory p)
+    case lookup (takeBaseName p) cs of
+        Just s  -> return $ Right s
+        Nothing -> return $ Left eNOENT
 
-mpdDirectory = emptyFileStat
+--
+-- File system description.
+--
+
+getDirectoryContents :: FilePath -> IO [(FilePath, FileStat)]
+getDirectoryContents p = join . liftM fromMPD . withMPD $ do
+    putStrLn $ "GETDIRECTORYCONTENTS " ++ p
+    -- NOTE: we make sure that paths begin with a slash for convenience.
+    case splitDirectories ("/" </> p) of
+        ("/":[]) -> return $ dots ++ [("Music", directory)
+                           ,("Outputs", directory)
+                           ,("Playlists", directory)
+                           ,("Status", directory)
+                           ,("Stats", directory)]
+        ("/":"Music":[]) -> do
+            return dots
+        ("/":"Outputs":[]) -> do
+            devs <- outputs
+            return $ dots ++ map (\x -> (deviceFileName x, mkFileStat (B.pack "0"))) devs
+        ("/":"Playlists":[]) -> do
+            pls <- lsPlaylists
+            return $ dots ++ map (\x -> (x, directory)) pls
+        ("/":"Status":[]) -> do
+            st <- status
+            return $ dots ++ [("state", mkFileStat (packInt $ stState st))
+                   ,("volume", mkFileStat (packInt $ stVolume st))
+                   ,("random_mode", mkFileStat (packInt $ stRandom st))
+                   ,("playlist_version", mkFileStat (packInt $ stPlaylistVersion st))
+                   ,("playlist_length", mkFileStat (packInt $ stPlaylistLength st))
+                   ,("song_pos", mkFileStat (packInt $ stSongPos st))
+                   ,("song_id", mkFileStat (packInt $ stSongID st))
+                   ,("time", mkFileStat (packInt $ stTime st))
+                   ,("bitrate", mkFileStat (packInt $ stBitrate st))
+                   ,("crossfade", mkFileStat (packInt $ stXFadeWidth st))
+                   ,("audio", mkFileStat (packInt $ stAudio st))
+                   ,("updating_db", mkFileStat (packInt $ stUpdatingDb st))
+                   ,("error", mkFileStat (B.pack $ stError st))]
+        ("/":"Stats":[]) -> do
+            sts <- stats
+            return $ dots ++ [("artists", mkFileStat (packInt $ stsArtists sts))
+                   ,("albums", mkFileStat (packInt $ stsAlbums sts))
+                   ,("songs", mkFileStat (packInt $ stsSongs sts))
+                   ,("uptime", mkFileStat (packInt $ stsUptime sts))
+                   ,("playtime", mkFileStat (packInt $ stsPlaytime sts))
+                   ,("db_playtime", mkFileStat (packInt $ stsDbPlaytime sts))
+                   ,("db_update", mkFileStat (packInt $ stsDbUpdate sts))]
+        _ -> fail "No such directory"
+    where dots = [(".", directory), ("..", directory)]
+
+-- Given a file content as a string, produce a file stat with appropriate size
+-- and block information.
+mkFileStat s = regularFile { statFileSize = fromIntegral len
+                           , statBlocks   = fromIntegral blk }
+    where
+        len = B.length s
+        blk = (len `div` 4096) + 1
+
+directory, regularFile, emptyStat :: FileStat
+directory = emptyStat
     { statEntryType = Directory
     , statFileSize = 4096
     , statBlocks = 1
+    , statFileMode = foldr1 unionFileModes [ ownerReadMode, ownerExecuteMode ]
     }
 
-mpdWritableFile = mpdReadOnlyFile `appendMode` allWriteMode
-
-mpdReadOnlyFile = emptyFileStat
+regularFile = emptyStat
     { statEntryType = RegularFile
-    , statFileMode = allReadMode
+    , statFileMode = foldr1 unionFileModes [ ownerReadMode, ownerWriteMode ]
     }
 
-emptyFileStat = FileStat
+emptyStat = FileStat
     { statEntryType = Unknown
+    , statFileMode = ownerModes
     , statLinkCount = 0
-    , statFileMode = 0
     , statFileOwner = 0
     , statFileGroup = 0
     , statSpecialDeviceID = 0
@@ -207,35 +229,32 @@ emptyFileStat = FileStat
     , statStatusChangeTime = 0
     }
 
-appendMode fstat mode =
-    fstat { statFileMode = combineModes [ mode, statFileMode fstat ] }
-
-allReadMode    = combineModes [ ownerReadMode, groupReadMode, otherReadMode ]
-allExecuteMode = combineModes [ ownerExecuteMode, groupExecuteMode, otherExecuteMode ]
-allWriteMode   = combineModes [ ownerWriteMode, groupWriteMode, otherWriteMode ]
-
-combineModes = foldr1 unionFileModes
-
 --
 -- Mapping MPD data structures to file system objects.
 --
 
-takeOutputID :: FilePath -> Int
-takeOutputID = read . take 1 . takeBaseName
+takeDeviceID :: FilePath -> Int
+takeDeviceID = read . take 1 . takeBaseName
 
-outputFileName :: Device -> FilePath
-outputFileName (Device i n _) = show i ++ ":" ++ replace ' ' '_' n
+deviceFileName :: Device -> FilePath
+deviceFileName (Device i n _) = show i ++ ":" ++ replace ' ' '_' n
 
 --
 -- Utilities.
 --
 
-replace :: Eq a => a -> a -> [a] -> [a]
-replace e with = map (\x -> if x == e then with else x)
+mpdToErrno :: Response a -> Either Errno a
+mpdToErrno (Left _)  = Left eNOENT
+mpdToErrno (Right r) = Right r
 
-trim :: ByteString -> ByteString
-trim = ltrim . rtrim
-    where
-        rtrim = B.reverse . ltrim . B.reverse
-        ltrim = B.dropWhile (`elem` chars)
-        chars = ['\n', '\r', ' ']
+fromMPD :: Response a -> IO a
+fromMPD = either (fail . show) return
+
+replace :: Eq a => a -> a -> [a] -> [a]
+replace from to = map (\x -> if x == from then to else x)
+
+anyPrefix :: Eq a => [a] -> [[a]] -> Bool
+anyPrefix s prefixes = any (`isPrefixOf` s) prefixes
+
+packInt :: Show a => a -> ByteString
+packInt = B.pack . show
